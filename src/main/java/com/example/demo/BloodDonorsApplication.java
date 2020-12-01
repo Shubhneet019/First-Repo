08335611cf1f59a558896f14c2fcfc0937c57240
Batra01 <gouravbatra43@gmail.com>
2020-12-01 23:42:36 +0530

    billa comment

--- src/main/java/com/example/demo/BloodDonorsApplication.java
+++ src/main/java/com/example/demo/BloodDonorsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BloodDonorsApplication {
-
+	// billa badmash
 	public static void main(String[] args) {
 		SpringApplication.run(BloodDonorsApplication.class, args);
 	}
